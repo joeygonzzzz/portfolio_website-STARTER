d39935ec6dc0090b78a219e2ccd1043a3a774ad2
joeygonzzzz <91030987+joeygonzzzz@users.noreply.github.com>
2022-04-12 19:46:36 -0500

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -8,7 +8,31 @@ const Footer = () => {
   return (
     <FooterWrapper>
       <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:830-965-2480">830-965-2480</LinkItem>
+        </LinkColumn>
+        <LinkColumn>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto: joey.o.gonzales@gmail.com">joey.o.gonzales@gmail.com</LinkItem>
+        </LinkColumn>
       </LinkList>
+      <SocialIconsContainer>
+        <CompanyContainer>
+          <Slogan>Slowly but surely learning my way as a developer!</Slogan>
+        </CompanyContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com">
+            <AiFillGithub size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://linkedin.com">
+            <AiFillLinkedin size="3rem" />
+          </SocialIcons>
+          <SocialIcons href="https://instagram.com">
+            <AiFillInstagram size="3rem" />
+          </SocialIcons>
+        </SocialContainer>
+      </SocialIconsContainer>
     </FooterWrapper>
   );
 };
